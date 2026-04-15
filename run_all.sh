961ba1a20f6bba774189f68cb831e186380821bd
Fred Xu <fredxu@squareup.com>
2026-04-15 14:21:10 -0700

    Remove one-off scripts, stale docs, and exploration code; keep core pipeline, transfer learning, and stochastic eval

--- run_all.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 nohup bash run_all_nxro.sh --members 100 --rollout_k 1 --epochs 500 --device auto --test --extra_train_nc auto &> log_nxro.txt &
-# CUDA_VISIBLE_DEVICES=1 nohup bash run_all_nxro.sh --stochastic --members 100 --rollout_k 1 --epochs 500 --device auto --test --extra_train_nc auto &> log_nxro_stochastic.txt &
-# CUDA_VISIBLE_DEVICES=2 nohup bash run_all_nxro_ora5.sh --members 100 --rollout_k 1 --epochs 2000 --device auto --test --topk 3 &> log_nxro_ora5.txt &
-# CUDA_VISIBLE_DEVICES=3 nohup bash run_all_nxro_ora5.sh --stochastic --members 100 --rollout_k 1 --epochs 2000 --device auto --test --topk 3 &> log_nxro_ora5_stochastic.txt &
